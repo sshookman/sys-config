3da31820634daf2236dc75cb23c30546b1471562
Sean Shookman <sms112788@gmail.com>
2016-07-03 10:39:25 -0500

    Adding git plugins and gitignore

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@ sudo apt install vim
 curl -sL https://raw.githubusercontent.com/egalpin/apt-vim/master/install.sh | sh
 apt-vim install -y https://github.com/scrooloose/nerdtree.git
 cd ~/.vim/bundle && git clone https://github.com/bling/vim-airline
+cd ~/.vim/bundle && git clone https://github.com/airblade/vim-gitgutter
+cd ~/.vim/bundle && git clone https://github.com/tpope/vim-fugitive
+vim -u NONE -c "helptags vim-fugitive/doc" -c q
 
 # Install xClip
 sudo apt-get install xclip
