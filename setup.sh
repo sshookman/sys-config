7e91ac936b1657f0679a7d00ae9f318f71934132
Sean Shookman <sms112788@gmail.com>
2017-04-18 16:33:50 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,11 +5,6 @@ sudo apt-get install git-all
 mkdir ~/Code
 cd ~/Code
 git clone git@github.com:sshookman/SysConfig.git
-git clone git@github.com:sshookman/ProjectHex.git
-git clone git@github.com:sshookman/ProjectRead.git
-git clone git@github.com:sshookman/ProjectNeo.git
-git clone git@github.com:sshookman/APODWallpaper.git
-git clone git@github.com:sshookman/Sandbox.git
 
 # Setup System Config
 cd ~/Code/SysConfig/scripts
