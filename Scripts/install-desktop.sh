662e1b7e05f974ef9b685e955b7c0c0a8f5bcf57
Sean Shookman <sms112788@gmail.com>
2017-04-20 19:06:33 +0000

    Fixes setup for ubuntu server

---
+++ Scripts/install-desktop.sh
@@ -0,0 +1,7 @@
+sudo apt-get install golang-go
+sudo apt-get install unity-tweak-tool
+sudo apt-get install synapse
+
+./install-terminator.sh
+./install-telegram.sh
+./install-soundsi.sh
--- Scripts/install.sh
+++ Scripts/install.sh
@@ -5,16 +5,10 @@ sudo apt-get install xclip
 sudo apt-get install tree
 sudo apt-get install glances
 sudo apt-get install weechat
-# sudo apt-get install golang-go
-# sudo apt-get install unity-tweak-tool
-# sudo apt-get install synapse
+sudo apt-get install python
 
 ./install-sudocabulary.sh
 ./install-vim.sh
-# ./install-terminator.sh
-# ./install-telegram.sh
-# ./install-soundsi.sh
 
 ./linkDotFiles.sh
-
 ./fix-wifi.sh
