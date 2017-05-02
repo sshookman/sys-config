3ba5df957d7068d5914787c6c643f6ac686d1144
Sean Shookman <sms112788@gmail.com>
2017-05-02 21:59:06 +0000

    Updates looping-downloader

--- Scripts/looping-downloader.sh
+++ Scripts/looping-downloader.sh
@@ -32,13 +32,22 @@ done
 
 let COUNTER=START+0
 let COUNT=COUNT+0
+let ZEROS=ZEROS+0
+
 while [ $COUNTER -lt $COUNT ]; do
     let COUNTER=COUNTER+1
     PRE=""
 
-    if [ $COUNTER -lt 10 ]; then
-        let PRE="0"
-    fi
+    let ZCOUNTER=0
+    while [ $ZCOUNTER -lt $ZEROS ]; do
+
+        let ZCOUNTER=ZCOUNTER+1
+        let DIGITS=10**$ZCOUNTER
+
+        if [ $COUNTER -lt $DIGITS ]; then
+            PRE=$PRE"0"
+        fi
+    done
 
     echo Downloading $URL$PRE$COUNTER$EXT
     curl --progress-bar -O $URL$PRE$COUNTER$EXT
