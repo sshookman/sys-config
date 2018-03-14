d00e74577fb665a44be0371cd447cb9c60717033
Sean Shookman <sms112788@gmail.com>
2018-03-13 21:24:47 -0500

    Fixes nerdtree install

--- Scripts/install-vim.sh
+++ Scripts/install-vim.sh
@@ -1,6 +1,6 @@
 sudo apt-get install vim
 curl -sL https://raw.githubusercontent.com/egalpin/apt-vim/master/install.sh | sh
-apt-vim install -y https://github.com/scrooloose/nerdtree.git
+cd ~/.vim/bundle && git clone https://github.com/scrooloose/nerdtree
 cd ~/.vim/bundle && git clone https://github.com/bling/vim-airline
 cd ~/.vim/bundle && git clone https://github.com/airblade/vim-gitgutter
 cd ~/.vim/bundle && git clone https://github.com/tpope/vim-fugitive
