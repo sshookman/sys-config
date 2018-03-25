314d3656968ec7763f52acd98ad220e09643e67f
Sean Shookman <sms112788@gmail.com>
2018-03-25 16:37:27 -0500

    Fixes path to scripts folder

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ cd ~/Code
 git clone git@github.com:sshookman/sys-config.git
 
 # Setup System Config
-cd ~/Code/SysConfig/Scripts
+cd ~/Code/sys-config/Scripts
 ./install.sh
 ./linkDotFiles.sh
 ./fix-wifi.sh
